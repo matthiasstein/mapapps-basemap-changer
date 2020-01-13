952f32fa35a3dfa09d11e75b960dde3dbc1681d8
Matthias Stein <m.stein@conterra.de>
2020-01-13 14:51:22 +0100

    Add basemapchanger bundle

---
+++ src/main/js/bundles/basemapchanger/nls/bundle.js
@@ -0,0 +1,14 @@
+module.exports = {
+    root: ({
+        bundleName: "BasemapChanger",
+        bundleDescription: "BasemapChanger",
+        ui: {
+            windowTitle: "BasemapChanger"
+        },
+        tool: {
+            title: "BasemapChanger",
+            tooltip: "BasemapChanger"
+        }
+    }),
+    "de": true
+};
---
+++ src/main/js/bundles/basemapchanger/nls/de/bundle.js
@@ -0,0 +1,11 @@
+module.exports = {
+    bundleName: "BasemapChanger",
+    bundleDescription: "BasemapChanger",
+    ui: {
+        windowTitle: "BasemapChanger"
+    },
+    tool: {
+        title: "BasemapChanger",
+        tooltip: "BasemapChanger"
+    }
+};
